2ef7c2df41909e343882c43cb647541648045fdc
magicbupt <caitingbupt@gmail.com>
2013-11-18 17:51:44 +0800

    change test

--- bin/libsvm_test.py
+++ bin/libsvm_test.py
@@ -10,6 +10,10 @@ y, x = [1,-1], [[1,0,1], [-1,0,-1]]
 y, x = [1,-1], [{1:1, 3:1}, {1:-1,3:-1}]
 prob  = svm_problem(y, x)
 param = svm_parameter('-t 0 -c 4 -b 1')
+
+
+
+#test is really ok?
 m = svm_train(prob, param)
 svm_save_model('../data/heart_scale.svm.model', m)
 m = svm_load_model('../data/heart_scale.svm.model')
